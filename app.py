e626ba52d5e4b13cf578d361ef5a057dc3b297fc
mameccof <84572511+mameccof@users.noreply.github.com>
2022-12-13 10:27:06 +0900

    [test] changes

--- app.py
+++ app.py
@@ -62,10 +62,14 @@ def handle_follow(event):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-  messages = TextSendMessage(text="AAA",
+  messages = TextSendMessage(text="コマンドを入力",
                               quick_reply=QuickReply(items=items))
 
+
+
   line_bot_api.reply_message(event.reply_token, messages=messages)
+
+
 
 
 if __name__ == "__main__":
